2691ed9a79651d3b97d378fc0b3b396670eb3044
Thanuj <dhinalrambukpotha@gmail.com>
2026-01-14 10:50:55 +0530

    Button Change for Away Board

--- count.js
+++ count.js
@@ -1,29 +1,47 @@
-const adding1 = 1
-const adding2 = 2
-const adding3 = 3
-
 let count = 0
+let awayCount = 0
 
 let Score = document.getElementById("amt-score")
+let awayScore = document.getElementById("away-score")
 
-function Score1(){
+function HScore1(){
     console.log("button clicked")
-    let tot1 = adding1 + count
+    let tot1 = 1 + count
     count = tot1
     Score.textContent = tot1
 }
 
-function Score2(){
+function HScore2(){
     console.log("button click2")
-    let tot2 = adding2 + count
+    let tot2 = 2 + count
     count = tot2
     Score.textContent = tot2
 }
 
-function Score3(){
+function HScore3(){
     console.log("button click3")
-    let tot3 = adding3 + count
-    count = tot3
+    let tot3 = 3 + awayCount
+    awayCount = tot3
     Score.textContent = tot3
 }
 
+function AScore1(){
+    console.log("button clicked")
+    let tot1 = 1 + awayCount
+    awayCount = tot1
+    awayScore.textContent = tot1
+}
+
+function AScore2(){
+    console.log("button click2")
+    let tot2 = 2 + awayCount
+    awayCount = tot2
+    awayScore.textContent = tot2
+}
+
+function AScore3(){
+    console.log("button click3")
+    let tot3 = 3 + awayCount
+    awayCount = tot3
+    awayScore.textContent = tot3
+}
